fca0461d51224196362f38a458e00e8d289f4bdd
Lương Minh Khôi <khoilm@aipower.com.vn>
2026-06-02 13:17:24 +0700

    feat(admin): add admin orders and order details pages (untested on real database)

--- src/main/java/com/sport/dao/OrderDAO.java
+++ src/main/java/com/sport/dao/OrderDAO.java
@@ -5,6 +5,8 @@ import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+import org.hibernate.Query;
 
 import com.sport.entity.Order;
 
@@ -17,5 +19,20 @@ public class OrderDAO {
 	public void saveOrder(Order order) {
 		Session session = sessionFactory.getCurrentSession();
 		session.save(order);
+	}
+	
+	@Transactional
+	@SuppressWarnings("unchecked")
+	public List<Order> getAllOrders() {
+		Session session = sessionFactory.getCurrentSession();
+		String hql = "FROM Order ORDER BY orderDate DESC";
+		Query query = session.createQuery(hql);
+		return query.list();
+	}
+	
+	@Transactional
+	public Order getOrderById(Integer id) {
+		Session session = sessionFactory.getCurrentSession();
+		return (Order) session.get(Order.class, id);
 	}
 }
